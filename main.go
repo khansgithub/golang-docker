3a7e4f9c9bb8e812ff3daba30bf546bcb880f952
khan <Khan.Ahmed@city.ac.uk>
2023-04-06 03:33:27 +0000

    update

--- main.go
+++ main.go
@@ -30,26 +30,45 @@ func check() {
 func setup_router() *gin.Engine {
 	router := gin.Default()
 	router.POST("/workflow/:workflow_name", post_root)
+	router.GET("/workflow/:container_id", get_container_output)
 	return router
+}
+
+func get_container_output(c *gin.Context){
+	ctx := context.Background()
+	
+	out, err := docker_cli.ContainerLogs(ctx, resp.ID, types.ContainerLogsOptions{
+		ShowStdout: true,
+		ShowStderr: true,
+	})
+	handle_ch_error(error_ch, err)
+	defer out.Close()
+
+	_, err = io.Copy(os.Stdout, out)
+	handle_ch_error(error_ch, err)
 }
 
 func post_root(c *gin.Context) {
 	defer check()
 
-	output_ch := make(chan string, 1)
+	container_id_ch := make(chan string, 1)
+	error_ch := make(chan error, 1)
 	workflow_name := c.Param("workflow_name")
-	error_group.Go(func() error {
-		return create_python_workflow(workflow_name, output_ch)
-	})
-	err := error_group.Wait()
-	handle_error(err)
-
-	fmt.Println(<-output_ch)
-
-	c.Status(http.StatusOK)
+	go create_python_workflow(workflow_name, container_id_ch, error_ch)
+	
+	conatiner_id := <-container_id_ch
+	c.String(http.StatusOK, conatiner_id)
 }
 
-func create_python_workflow(workflow_name string, output_ch chan string) error {
+func handle_ch_error(error_ch chan error, err error){
+	if err != nil{
+		error_ch<-err
+		panic(err)
+	}
+}
+
+func create_python_workflow(workflow_name string, container_id_ch chan string, error_ch chan error){
+	output_ch := make(chan string, 1)
 	defer close(output_ch)
 	ctx := context.Background()
 	container_configs := NewContainerConfig(workflow_name)
@@ -60,14 +79,26 @@ func create_python_workflow(workflow_name string, output_ch chan string) error {
 		nil, nil,
 		container_configs.ContainerName,
 	)
-	handle_error(err)
+	handle_ch_error(error_ch, err)
 
-	output_ch <- ">>>>>>> Container ID: " + resp.ID
+	containers[resp.ID] = output_ch
+	container_id_ch <- resp.ID
+	close(container_id_ch)
 
 	err = docker_cli.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{})
-	handle_error(err)
+	handle_ch_error(error_ch, err)
 
-	return err
+	// statusCh, errCh := docker_cli.ContainerWait(ctx, resp.ID, container.WaitConditionNextExit)
+	// select {
+	// case err := <-errCh:
+	// 	handle_ch_error(error_ch, err)
+	// case status := <-statusCh:
+	// 	// Print the exit code of the container
+	// 	fmt.Printf("Container exited with status %d\n", status.StatusCode)
+	// }
+
+	output_ch <- ">>>>>>> End"
+	// return err
 }
 
 type ContainerConfigs struct {
@@ -95,6 +126,75 @@ func NewContainerConfig(workflow_name string) *ContainerConfigs {
 
 	return &ContainerConfigs{config, hostConfig, containerName}
 }
+
+func clone_repo() {
+	repoURL := "ssh://git@localhost:2220/srv/repo"
+	cloneDir := "/clone_dir"
+
+	password := "123"
+
+	// Create an Auth object with password
+	auth := &ssh.Password{
+		User:     "git", // Replace with your username
+		Password: password,
+	}
+
+	// Clone the repository with the provided authentication
+	_, err := git.PlainClone(cloneDir, false, &git.CloneOptions{
+		URL:  repoURL,
+		Auth: auth,
+	})
+
+	if err != nil {
+		fmt.Println("Failed to clone repository:", err)
+		os.Exit(1)
+	}
+}
+
+func pull_images() {
+	fmt.Println("Pulling python:latest ...")
+	ctx := context.Background()
+	reader, err := docker_cli.ImagePull(ctx, "python:latest", types.ImagePullOptions{})
+	if err != nil {
+		panic(err)
+	}
+	defer reader.Close()
+
+	// Print the output from the pull
+	buf := make([]byte, 4096)
+	for {
+		_, err := reader.Read(buf)
+		if err != nil {
+			break
+		}
+	}
+	fmt.Println("Image pulled")
+}
+
+var error_group *errgroup.Group
+var docker_cli *client.Client
+var containers map[string]chan string
+
+func main() {
+	// docker_cli()
+	// clone_repo()
+	var err error
+	containers = make(map[string]chan string)
+	error_group = new(errgroup.Group)
+	docker_cli, err = client.NewClientWithOpts(client.FromEnv)
+	handle_error(err)
+	pull_images()
+
+	r := setup_router()
+	r.Run(":8080")
+}
+
+func handle_error(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
 
 func docker_cli_f() {
 	cli, err := client.NewClientWithOpts(client.FromEnv)
@@ -162,70 +262,4 @@ func docker_cli_f() {
 	// if _, err := io.Copy(os.Stdout, out); err != nil {
 	// 	panic(err)
 	// }
-}
-
-func clone_repo() {
-	repoURL := "ssh://git@localhost:2220/srv/repo"
-	cloneDir := "/clone_dir"
-
-	password := "123"
-
-	// Create an Auth object with password
-	auth := &ssh.Password{
-		User:     "git", // Replace with your username
-		Password: password,
-	}
-
-	// Clone the repository with the provided authentication
-	_, err := git.PlainClone(cloneDir, false, &git.CloneOptions{
-		URL:  repoURL,
-		Auth: auth,
-	})
-
-	if err != nil {
-		fmt.Println("Failed to clone repository:", err)
-		os.Exit(1)
-	}
-}
-
-func pull_images(){
-	fmt.Println("Pulling python:latest ...")
-	ctx := context.Background()
-	reader, err := docker_cli.ImagePull(ctx, "python:latest", types.ImagePullOptions{})
-	if err != nil {
-		panic(err)
-	}
-	defer reader.Close()
-
-	// Print the output from the pull
-	buf := make([]byte, 4096)
-	for {
-		_, err := reader.Read(buf)
-		if err != nil {
-			break
-		}
-	}
-	fmt.Println("Image pulled")
-}
-
-var error_group *errgroup.Group
-var docker_cli *client.Client
-
-func main() {
-	// docker_cli()
-	// clone_repo()
-	var err error
-	error_group = new(errgroup.Group)
-	docker_cli, err = client.NewClientWithOpts(client.FromEnv)
-	handle_error(err)
-	pull_images()
-
-	r := setup_router()
-	r.Run(":8080")
-}
-
-func handle_error(err error) {
-	if err != nil {
-		panic(err)
-	}
 }
